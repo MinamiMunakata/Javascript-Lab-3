ac2502ff2800adfa4bab791bb0e3b5b99d15f2bc
Minami Munakata <june28munazo@gmail.com>
2018-02-09 00:10:38 -0800

    homework by Changami san, get position, change the order in the array, change color, get start time, count movement

--- app.js
+++ app.js
@@ -1,46 +1,156 @@
-var puzArr = [[1,2,3],[4,5,6],[7,8,0]]
-console.log(puzArr)
+var puzArr = [[1,2,3],[4,5,6],[7,8,0]];
 
-1 = 0,0
-2 = 0,1
-3 = 0,2
-4 = 1,0
-5 = 1,1
-6 = 1,2
-7 = 2,0
-8 = 2,1
-0 = 2,2
+
+var obj = {
+
+};
+var zero = {
+
+};
+
+var count = 0;
+
+//isMovable(obj.row, obj.column)
+
+function move(number){
+	if (number <= 3) {
+		obj.row = 0
+	} else if (number >= 4 && number <= 6) {
+		obj.row = 1
+	} else {
+		obj.row = 2
+	}
+	if ((number+2)%3 == 0 ) {
+		obj.column = 0
+	} else if ((number+1)%3 == 0) {
+		obj.column = 1
+	} else {
+		obj.column = 2
+	}
+	console.log(obj)
+	if (isMovable(obj.row,obj.column)) {
+		puzArr[zero.row][zero.column] = puzArr[obj.row][obj.column];
+		puzArr[obj.row][obj.column] = 0;
+		count += 1
+		document.getElementById("move_count").innerHTML = "Moves: " + count; // -- Done!!
+		console.log(puzArr);
+		console.log(count)
+
+	}
+};
+
+
+ // numberの座標を取る method
+// function getRowColumn(number) {
+// 	if (number <= 3) {
+// 		obj.row = 0
+// 	} else if (number >= 4 && number <= 6) {
+// 		obj.row = 1
+// 	} else {
+// 		obj.row = 2
+// 	}
+// 	if ((number+2)%3 == 0 ) {
+// 		obj.column = 0
+// 	} else if ((number+1)%3 == 0) {
+// 		obj.column = 1
+// 	} else {
+// 		obj.column = 2
+// 	}
+// 	console.log(obj)
+// }
+
+ //　とった座標をisMovableで取得する
+ // isMovable trueのとき入れ替える swap
+ // how to animate?
+
+
 function isMovable(row,column) {
+	console.log((isMovableUp(row,column) || isMovableRight(row,column) || isMovableDown(row,column) || isMovableLeft(row,column)))
+	return (isMovableUp(row,column) || isMovableRight(row,column) || isMovableDown(row,column) || isMovableLeft(row,column))
 	console.log(puzArr[row-1][column]) //up 
 	console.log(puzArr[row][column+1]) // right
 	console.log(puzArr[row+1][column]) // down
 	console.log(puzArr[row][column-1]) // left
 }
 
-5 = 1,1
-up = [0][1] = i-1, i
-down = [2][1] = i+1, i
-left = [1][0] = i, i-1
-right = [1][2] = i, i+1
+function isMovableUp(row,column){
+	if (row != 0 && !puzArr[row -1][column]) {
+		zero.row = row-1;
+		zero.column = column;
+		return true;
+	}
+}
+
+function isMovableRight(row,column){
+	if (column < 2 && !puzArr[row][column +1]) {
+		zero.row = row;
+		zero.column = column+1;
+		return true;
+	}
+}
+
+function isMovableDown(row,column){
+	if (row < 2 && !puzArr[row +1][column]) {
+		zero.row = row+1;
+		zero.column = column;		
+		return true;
+	}
+}
+
+function isMovableLeft(row,column){
+	if (column != 0 && !puzArr[row][column -1]) {
+		zero.row = row;
+		zero.column = column-1;		
+		return true;
+	}	
+}
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var date = new Date();
+    var x = document.getElementById("start_time");
+    var h = addZero(date.getHours());
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+    x.innerHTML = "Start Time: " + h + ":" + m + ":" + s;
+}
 
 
-[[1,2,3],
-[4,5,6],
-[7,8,0]]
+// how to get it only for the first time 
 
-class = up
-class = down
-class = left
-class = right
+function mouseOver(number){
+	if (number <= 3) {
+		obj.row = 0
+	} else if (number >= 4 && number <= 6) {
+		obj.row = 1
+	} else {
+		obj.row = 2
+	}
+	if ((number+2)%3 == 0 ) {
+		obj.column = 0
+	} else if ((number+1)%3 == 0) {
+		obj.column = 1
+	} else {
+		obj.column = 2
+	}
 
-$(function(){
+	var idName = "tile" + number;
+	if (isMovable(obj.row,obj.column)) {
+		document.getElementById(idName).style.backgroundColor = "lightgreen";
 
-})
+	} else {
+		document.getElementById(idName).style.backgroundColor = "red";
+	}
+};
 
-move(array,row,col)
-
-
-
-function move(array,row,col) {
-
+// bg color
+function moveOut(number) {
+	var idName = "tile" + number;
+    document.getElementById(idName).style.backgroundColor = "";
 }
