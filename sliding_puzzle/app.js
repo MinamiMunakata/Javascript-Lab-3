575579a139e0e9fd947a964a052046f6188d700b
Minami Munakata <june28munazo@gmail.com>
2018-02-09 16:13:40 -0800

    大上さんと一緒にやったところ、バグあり。課題：ランダム、クリア判定

--- sliding_puzzle/app.js
+++ sliding_puzzle/app.js
@@ -1,98 +1,90 @@
-//--------開始時間
-// var st = true;
-function startTime() {
-	// if (st) {
-	    var date = new Date();
-	    var x = document.getElementById("start_time");
-	    var h = addZero(date.getHours());
-	    var m = addZero(date.getMinutes());
-	    var s = addZero(date.getSeconds());
-	    x.innerHTML = "Start Time: " + h + ":" + m + ":" + s;	
-	// }
-	// st = false;	
+//bug あり　入れ替わりがおかしい　６　３　２　５　！３でバグ！
+// function () {
 
-}
+// }
+
+// らんだむ
+// すたーと
+// カウント
+// リスタートでリセットされるもの
+
+// クリア判定
+// Click event "" 書き換える
 
 var puzArr = [[1,2,3],[4,5,6],[7,8,0]];
-
-
-var obj = {
-
-};
-var zero = {
-
-};
-
+var zero = {};
 var count = 0;
 
-//isMovable(obj.row, obj.column)
+function upDateDisplay() {
+	var $tiles = $("#tiles")
+	var inputToUl = ""
+	var tileOrder =	getTileOrderAsArray()
 
-function move(number){
+	for (var tileNumber = 0; tileNumber < tileOrder.length; tileNumber++) {
+		if (tileOrder[tileNumber] == 0) {
+			inputToUl +=' <li class="tile" id="hidden">0</li>'
+		} else {
+			inputToUl +=' <li class="tile" id="tile' + tileOrder[tileNumber] + '" onClick="move(' + tileOrder[tileNumber] + ')" onmouseover="mouseOver(' + tileOrder[tileNumber] + ')" onmouseout="moveOut(' + tileOrder[tileNumber] + ')">' + tileOrder[tileNumber] + '</li>'
+		}
+	}
+	$tiles.html(inputToUl)
+}
+
+function startTime() {
+    var date = new Date();
+    var x = document.getElementById("start_time");
+    var h = addZero(date.getHours());
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+    x.innerHTML = "Start Time: " + h + ":" + m + ":" + s;
+}
+
+function getTileOrderAsArray() {
+	var tileOrder = [];
+
+	for (var row = 0; row < puzArr.length ; row++) {
+	for (var column = 0; column < puzArr[row].length ; column++) {
+		tileOrder.push(puzArr[row][column]);
+	}
+	}
+	return tileOrder;
+}
+
+function getPoint(number){
+	var row;
+	var column;
+
 	if (number <= 3) {
-		obj.row = 0
+		row = 0
 	} else if (number >= 4 && number <= 6) {
-		obj.row = 1
+		row = 1
 	} else {
-		obj.row = 2
+		row = 2
 	}
 	if ((number+2)%3 == 0 ) {
-		obj.column = 0
+		column = 0
 	} else if ((number+1)%3 == 0) {
-		obj.column = 1
+		column = 1
 	} else {
-		obj.column = 2
+		column = 2
 	}
-	console.log(obj)
+	return {"row": row, "column": column}
+}
+
+function move(number){
+	var obj = getPoint(number)
+
 	if (isMovable(obj.row,obj.column)) {
 		puzArr[zero.row][zero.column] = puzArr[obj.row][obj.column];
 		puzArr[obj.row][obj.column] = 0;
 		count += 1
 		document.getElementById("move_count").innerHTML = "Moves: " + count; // -- Done!!
-		console.log(puzArr);
-		console.log(count)
-		// //---------------HTML を書き換える？？
-		// var list = document.getElementById(idName).innerHTML
-		// console.log(list)
-		// var idName = "tile" + number;
-		// console.log(idName)
-		// document.getElementById(idName).innerHTML = "0";
-  //   	document.getElementById(idName).id = "hidden";
-  //   	document.getElementById("hidden").innerHTML = number;
-  //   	document.getElementById("hidden").id = idName;
-  //   	console.log(idName)
-
+		upDateDisplay(); //表示画面更新する
 	}
 };
 
-
-// numberの座標を取る method
-// function getRowColumn(number) {
-// 	if (number <= 3) {
-// 		obj.row = 0
-// 	} else if (number >= 4 && number <= 6) {
-// 		obj.row = 1
-// 	} else {
-// 		obj.row = 2
-// 	}
-// 	if ((number+2)%3 == 0 ) {
-// 		obj.column = 0
-// 	} else if ((number+1)%3 == 0) {
-// 		obj.column = 1
-// 	} else {
-// 		obj.column = 2
-// 	}
-// 	console.log(obj)
-// }
-
-
-
 function isMovable(row,column) {
-	console.log((isMovableUp(row,column) || isMovableRight(row,column) || isMovableDown(row,column) || isMovableLeft(row,column)))
 	return (isMovableUp(row,column) || isMovableRight(row,column) || isMovableDown(row,column) || isMovableLeft(row,column))
-	console.log(puzArr[row-1][column]) //up 
-	console.log(puzArr[row][column+1]) // right
-	console.log(puzArr[row+1][column]) // down
-	console.log(puzArr[row][column-1]) // left
 }
 
 function isMovableUp(row,column){
@@ -102,6 +94,7 @@ function isMovableUp(row,column){
 		return true;
 	}
 }
+
 
 function isMovableRight(row,column){
 	if (column < 2 && !puzArr[row][column +1]) {
@@ -134,37 +127,17 @@ function addZero(i) {
     return i;
 }
 
-
-
-// how to get it only for the first time 
-
 function mouseOver(number){
-	if (number <= 3) {
-		obj.row = 0
-	} else if (number >= 4 && number <= 6) {
-		obj.row = 1
-	} else {
-		obj.row = 2
-	}
-	if ((number+2)%3 == 0 ) {
-		obj.column = 0
-	} else if ((number+1)%3 == 0) {
-		obj.column = 1
-	} else {
-		obj.column = 2
-	}
-
+	var obj = getPoint(number)
 	var idName = "tile" + number;
+
 	if (isMovable(obj.row,obj.column)) {
 		document.getElementById(idName).style.backgroundColor = "lightgreen";
-
 	} else {
 		document.getElementById(idName).style.backgroundColor = "red";
 	}
 };
-
-// bg color
 function moveOut(number) {
-	var idName = "tile" + number;
-    document.getElementById(idName).style.backgroundColor = "";
+		var idName = "tile" + number;
+	    document.getElementById(idName).style.backgroundColor = "";
 }
